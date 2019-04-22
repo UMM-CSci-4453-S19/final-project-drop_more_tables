2d7b4227efedb38f59796d010852393ba8819313
EthanUphoff <ethan.uphoff@gmail.com>
2019-04-22 16:51:17 -0500

    Check for 404 error on website for image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 mysql = require('mysql')
 credentials = require('./credentials.json')
+const rp = require('request-promise')
 
 credentials.database = "create_tables"
 credentials.host = 'ids.morris.umn.edu'
@@ -19,8 +20,25 @@ bot.on('message', function (msg) {
 		msgcon = msg.content.toLowerCase()
 		const args = msgcon.slice(prefix.length).trim().split(/ +/g)
 		console.log(args)
-		if (args[0] === "pokemon" && args[1] !== '') {
-			msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani/" + args[1] + ".gif")
+		if (args[0] === "pokemon" && args[1]) {
+			//msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani/" + args[1] + ".gif")
+      check404(msg, args[1]);
 		}
+    if (args[0] === "shiny" && args[1]){
+      msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani-shiny/" + args[1] + ".gif")
+    }
 	}
 })
+
+function check404(msg, pokemon){
+  const options = {
+    uri: "http://play.pokemonshowdown.com/sprites/xyani/" + pokemon + ".gif"
+  }
+
+  rp(options)
+    .then(($) => {
+        msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani/" + pokemon + ".gif")
+    }).catch((err) => {
+      msg.channel.send("Pokemon not found")
+    })
+}
