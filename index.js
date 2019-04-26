a956ba4e14902ee9453c09f1ca0eea9981368a6e
EthanUphoff <ethan.uphoff@gmail.com>
2019-04-26 16:18:41 -0500

    Add SQL queries for pokemon name

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const Discord = require('discord.js')
 mysql = require('mysql')
 credentials = require('./credentials.json')
 const rp = require('request-promise')
+port = process.env.PORT || 1337;
 
 credentials.database = "create_tables"
-credentials.host = 'ids.morris.umn.edu'
+credentials.host='ids.morris.umn.edu'; //setup database credentials
+
+var connection = mysql.createConnection(credentials); // setup the connection
+
+connection.connect(function(err){if(err){console.log(err)}});
+
 const bot = new Discord.Client()
 var prefix = "!pd"
 
@@ -22,7 +28,28 @@ bot.on('message', function (msg) {
 		console.log(args)
 		if (args[0] === "pokemon" && args[1]) {
 			//msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani/" + args[1] + ".gif")
-      check404(msg, args[1]);
+      //check404(msg, args[1]);
+			var pokemon = args[1];
+			console.log(pokemon)
+			var sql = 'SELECT stat_identifier FROM pokemon where stat_identifier = ?';
+		  connection.query(sql, pokemon,(function(msg){return function(err,rows,fields){
+				console.log(msg)
+				console.log(sql)
+		     var dbfarr = new Array(rows.length);
+		     // Loop over the response rows and put the information into an array of maps
+		     // We can then use this to create our buttons
+
+		     rows.forEach(function (item, index) {
+					 console.log(item)
+		       dbfarr[index] = item.stat_identifier;
+		  });
+			console.log(dbfarr[0])
+		     if(err){
+		       console.log("We have an error:");
+		       console.log(err);
+		     }
+				 msg.channel.send(dbfarr[0])
+		  }})(msg));
 		}
     if (args[0] === "shiny" && args[1]){
       msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani-shiny/" + args[1] + ".gif")
