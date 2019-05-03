9fed6ea7108de35cc32e6d28ffc8d944954bbc06
EthanUphoff <ethan.uphoff@gmail.com>
2019-05-02 23:05:53 -0500

    update messages

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('message', function (msg) {
 		console.log(args)
 		if (args[0] === "pokemon" && args[1]) {
 			//msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani/" + args[1] + ".gif")
-      //check404(msg, args[1]);
+			//check404(msg, args[1]);
 			var pokemon = args[1];
 			var x = 3;
 			var info = '';
@@ -41,10 +41,10 @@ bot.on('message', function (msg) {
 					var abilityinfo = result
 					geteachability(msg, info, abilityinfo, function(result){
 						console.log(result)
-						sendpokemon(msg, info);
+						sendpokemon(msg, info, result);
 					})
 					//result.forEach(function(item, index){
-						//abilityarr[index] = getabilities(item.ability_id);
+					//abilityarr[index] = getabilities(item.ability_id);
 					//})
 					//var abilities = processabilities(abilityarr)
 					//console.log(abilities)
@@ -55,64 +55,64 @@ bot.on('message', function (msg) {
 				})
 			})
 		}
-    if (args[0] === "shiny" && args[1]){
-      msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani-shiny/" + args[1] + ".gif")
-    }
+		if (args[0] === "shiny" && args[1]){
+			msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani-shiny/" + args[1] + ".gif")
+		}
 	}
 })
 
 function check404(msg, pokemon){
-  const options = {
-    uri: "http://play.pokemonshowdown.com/sprites/xyani/" + pokemon + ".gif"
-  }
+	const options = {
+		uri: "http://play.pokemonshowdown.com/sprites/xyani/" + pokemon + ".gif"
+	}
 
-  rp(options)
-    .then(($) => {
-        msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani/" + pokemon + ".gif")
-    }).catch((err) => {
-      msg.channel.send("Pokemon not found")
-    })
+	rp(options)
+	.then(($) => {
+		msg.channel.send("http://play.pokemonshowdown.com/sprites/xyani/" + pokemon + ".gif")
+	}).catch((err) => {
+		msg.channel.send("Pokemon not found")
+	})
 }
 
 function pokemongeninfo(msg, pokemon, callback){
 	var sql = 'SELECT * FROM pokemon where stat_identifier = ?';
 	connection.query(sql, pokemon,function(err,rows,fields){
-		     var dbfarr = new Array(rows.length);
-		     // Loop over the response rows and put the information into an array of maps
-		     // We can then use this to create our buttons
+		var dbfarr = new Array(rows.length);
+		// Loop over the response rows and put the information into an array of maps
+		// We can then use this to create our buttons
 
-		     rows.forEach(function (item, index) {
-		       dbfarr[index] = {"name" : item.stat_identifier,
-		       					"species_id" : item.species_id,
-		       					"id" : item.id
-		      				   };
-		  });
-			//console.log(dbfarr[0])
-		     if(err){
-		       console.log("We have an error:");
-		       console.log(err);
-		     }
-				 return callback(dbfarr[0])
-		  });
+		rows.forEach(function (item, index) {
+			dbfarr[index] = {"name" : item.stat_identifier,
+			"species_id" : item.species_id,
+			"id" : item.id
+		};
+	});
+	//console.log(dbfarr[0])
+	if(err){
+		console.log("We have an error:");
+		console.log(err);
+	}
+	return callback(dbfarr[0])
+});
 }
 
 function pokemonabilitiesinfo(msg, info, id, callback){
 	var sql = 'SELECT * FROM pokemon_abilities where pokemon_id = ?';
 	connection.query(sql, id,function(err,rows,fields){
-		     var dbfarr = new Array(rows.length);
-		     // Loop over the response rows and put the information into an array of maps
-		     // We can then use callbackthis to create our buttons
+		var dbfarr = new Array(rows.length);
+		// Loop over the response rows and put the information into an array of maps
+		// We can then use callbackthis to create our buttons
 
-		     rows.forEach(function (item, index) {
-		       dbfarr[index] = item.ability_id
-		  });
-			//console.log(dbfarr)
-		     if(err){
-		       console.log("We have an error:");
-		       console.log(err);callback
-		     }
-				 return callback(dbfarr)
-		  });
+		rows.forEach(function (item, index) {
+			dbfarr[index] = item.ability_id
+		});
+		//console.log(dbfarr)
+		if(err){
+			console.log("We have an error:");
+			console.log(err);callback
+		}
+		return callback(dbfarr)
+	});
 }
 
 function processabilities(abilityarr){
@@ -124,76 +124,88 @@ function processabilities(abilityarr){
 }
 
 /*function geteachability(msg, info, abilityarr, callback){
-	var abilities = new Array(callbackabilityarr.length)
-	var itemsProcessed = 0;
-	abilityarr.forEach((item, index) => {
-		AsyncFunction(item, index, () => {
-			abilities[index] = getabilities(item.ability_id);
-			itemsProcessed++;
-			if(itemsProcessed === abilityarr.length){return callback(abilities)}
-		})callback
+var abilities = new Array(callbackabilityarr.length)
+var itemsProcessed = 0;
+abilityarr.forEach((item, index) => {
+AsyncFunction(item, index, () => {
+abilities[index] = getabilities(item.ability_id);
+itemsProcessed++;
+if(itemsProcessed === abilityarr.length){return callback(abilities)}
+})callback
 })
 }*/
 
 function geteachability(msg, info, abilityarr, callback){
 	var abilities = new Array(abilityarr.length)
 	/*abilityarr.forEach(function(item, index){
-		console.log(item.ability_id)
-		/*getabilities(item.ability_id, index, function(result){
-			//console.log("Hello")
-			abilities[index] = result
-			//console.log(index)
-			//console.log(result)
-			//console.log(abilities[index])
-			console.log(abilities)
-		})
-			abilities[index] = getabilities(item.ability_id);
-			var i = getabilities(item.ability_id);
-			console.log(i)
-			console.log(getabilities(item.ability_id))
-		})
-		console.log(abilities)*/
-		getabilities(abilities, abilityarr, function(result){
-			return callback(result);
-		})
+	console.log(item.ability_id)
+	/*getabilities(item.ability_id, index, function(result){
+	//console.log("Hello")
+	abilities[index] = result
+	//console.log(index)
+	//console.log(result)
+	//console.log(abilities[index])
+	console.log(abilities)
+})
+abilities[index] = getabilities(item.ability_id);
+var i = getabilities(item.ability_id);
+console.log(i)
+console.log(getabilities(item.ability_id))
+})
+console.log(abilities)*/
+getabilities(abilities, abilityarr, function(result){
+	return callback(result);
+})
 }
 
 function getabilities(abilities, id, callback){
-	var sql = '';
-	if(id.length = 3){
+	var sql = 'SELECT * FROM abilities where';
+if(id.length = 3){
 	var sql = 'SELECT * FROM abilities where id = ? or id = ? or id = ?';
 } else if (id.length = 2){
-	var sql = 'SELECT * FROM abilities where id = ? or id = ?';
+var sql = 'SELECT * FROM abilities where id = ? or id = ?';
 } else if (id.length = 1){
-	var sql = 'SELECT * FROM abilities where id = ?';
-}
-	console.log(id)
-	//console.log(id)
-	connection.query(sql, id, function(err,rows,fields){
-		//console.log(sql)
-		     var dbfarr = new Array(rows.length);
-		     // Loop over the response rows and put the information into an array of maps
-		     // We can then use this to create our buttons
-
-		     rows.forEach(function (item, index) {
-		       dbfarr[index] = {"identifier" : item.identifier};
-		  });
-			//console.log(dbfarr)
-		     if(err){
-		       console.log("We have an error:");
-		       console.log(err);
-		     }
-				 console.log(dbfarr)
-				 return callback(dbfarr);
-				 //return dbfarr[0]
-		  });
+var sql = 'SELECT * FROM abilities where id = ?';
 }
 
-function sendpokemon(msg, pokemon){
+console.log(id)
+//console.log(id)
+connection.query(sql, id, function(err,rows,fields){
+	//console.log(sql)
+	var dbfarr = new Array(rows.length);
+	// Loop over the response rows and put the information into an array of maps
+	// We can then use this to create our buttons
+
+	rows.forEach(function (item, index) {
+		dbfarr[index] = {"identifier" : item.identifier};
+	});
+	//console.log(dbfarr)
+	if(err){
+		console.log("We have an error:");
+		console.log(err);
+	}
+	console.log(dbfarr)
+	return callback(dbfarr);
+	//return dbfarr[0]
+});
+}
+
+function sendpokemon(msg, pokemon, abilities){
 	console.log(pokemon)
 	var embedmsg = new Discord.RichEmbed()
-		.setTitle(pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1))
-		.setImage("http://play.pokemonshowdown.com/sprites/xyani/" + pokemon.name + ".gif")
+	.setTitle(pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1))
+	.setImage("http://play.pokemonshowdown.com/sprites/xyani/" + pokemon.name + ".gif")
+	.addField("Abilities:\n" + abilitiesstr(abilities))
 
 	msg.channel.send(embedmsg)
+}
+
+function abilitiesstr(arr){
+	var abilities = ''
+	arr.forEach(function(item, index){
+		abilities = abilities + item.identifier + '\n';
+	})
+	console.log(abilities)
+	console.log(arr)
+	return abilities;
 }
